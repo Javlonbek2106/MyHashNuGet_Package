37f1ada617934aef550cd4c676f5416fc4caa5bf
Javlonbek <122851751+Javlonbek2106@users.noreply.github.com>
2023-08-25 22:29:03 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            
+        }
+        public void Get(in int num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ HashNuGet/HashSHA256.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HashNuGet
+{
+    public static class HashSHA256
+    {
+        public static string GetHashedString(this string text)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+                byte[] hashbytes = sha256.ComputeHash(bytes);
+                text = Convert.ToBase64String(hashbytes);
+
+            }
+            return text;
+        }
+    }
+}
